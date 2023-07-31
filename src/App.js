d25e39df28c39df0d84e42cdc98c4c62e4f01aaa
HMT1140 <139555499+HMT1140@users.noreply.github.com>
2023-07-31 18:56:32 +0500

    Navbar main layout done without darkmode button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Navbar from './components/Main components/Navbar';
+import Home from './components/Main components/Home';
+import Shop from './components/Main components/Shop';
+import Contactus from './components/Main components/Contactus'
+import ShoppingCart from './components/Checkout/Shoppingcart'
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Navbar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/Shop' element={<Shop/>}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/Contactus' element={<Contactus/>}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/shoppingcart' element={<ShoppingCart/>}></Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
